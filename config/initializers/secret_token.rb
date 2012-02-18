9421d95da5215ab8db7e87e9804e7683d234f621
Denis <denis.bollinne@gmail.com>
2012-02-18 09:17:30 +0100

    new app again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OurAwesomeGame::Application.config.secret_token = 'db682447a220148b63d13e7f2b7f15784a693fd39e8959bd7c620d05c67b4fe26493c607ee1ba7ae69c5d855f6c2fc05c48a709921f8cd2e42189927e57477e0'
