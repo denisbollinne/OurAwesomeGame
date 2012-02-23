42b7b8b263d1fc8c70b3da692a24fecfe8b77f3f
Denis Bollinne <denis.bollinne@itense.be>
2012-02-23 22:19:10 +0100

    did a fancy animation :p

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,22 +3,27 @@
  */
 
 function Dot(gs) {
-    var x = gs.width * 0.5;
-    var y = gs.height * 0.5;
-    var r = gs.width * 0.1;
+	var x = gs.width * 0.5;
+	var y = gs.height * 0.5;
+	var r = gs.width * 0.1;
+	var i = 0;
+	var iY = 0;
+	this.update = function() {
+		x += gs.width * 0.02 * (Math.cos(i));
+		y += gs.height * 0.04 * (Math.sin(iY));
+		i += 0.1;
+		iY += 0.2;
+		if(i > 100000000) i = 0;
+		if(iY > 100000000) iY = 0;
+	}
 
-    this.update = function() {
-        x += gs.width * 0.01 * (Math.random() - 0.5);
-        y += gs.height * 0.01 * (Math.random() - 0.5);
-    }
-
-    this.draw = function(c) {
-        c.fillRect(x - r / 2, y - r / 2, r, r);
-    }
+	this.draw = function(c) {
+		c.fillRect(x - r / 2, y - r / 2, r, r);
+	}
 }
 
 function startGame() {
-    var gs = new JSGameSoup("surface", 30);
-    gs.addEntity(new Dot(gs));
-    gs.launch();
+	var gs = new JSGameSoup("surface", 30);
+	gs.addEntity(new Dot(gs));
+	gs.launch();	
 }
